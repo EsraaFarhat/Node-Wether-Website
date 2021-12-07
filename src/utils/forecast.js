b70974c193357c4f7877fb6e7d7fb430ce06b76c
EsraaFarhat <esraafarhat97@gmail.com>
2021-12-07 02:19:05 +0200

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require("request");
 
+
+
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=90b4fd8cf4bb6109bd9ef45b073cf35d&query=${latitude},${longitude}&units=f`;
 
@@ -11,7 +13,8 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${response.body.current.weather_descriptions[0]}. It's currently ${response.body.current.temperature} degrees outside. It feels like ${response.body.current.feelslike} degrees outside.`
+        `${response.body.current.weather_descriptions[0]}. It's currently ${response.body.current.temperature} degrees outside. 
+        It feels like ${response.body.current.feelslike} degrees outside. The humidity is ${response.body.current.humidity}%.`
       );
     }
   });
